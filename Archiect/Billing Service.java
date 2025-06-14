2e713b7d1f2cc2d2003a70e06d407026535c5fa1
Tanzeela Fatima <149133736+Fatima-progmmer@users.noreply.github.com>
2025-06-14 08:32:35 +0500

    Add files via upload

---
+++ Archiect/Billing Service.java
@@ -0,0 +1,44 @@
+
+
+/**
+ * @author Kashmiri Queen
+ * @version 1.0
+ * @created 17-May-2025 01:39:39 Fatima
+ */
+public class Billing Service {
+
+	private Double Amount;
+	private int Bill_ID;
+	public Billing Service m_Billing Service;
+
+	public Billing Service(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	/**
+	 * 
+	 * @param Parameter A
+	 */
+	public void Billing_gateway(int Parameter A){
+
+	}
+
+	/**
+	 * 
+	 * @param Parameter
+	 */
+	public void Calculate_bill(int Parameter){
+
+	}
+
+	/**
+	 * 
+	 * @param void
+	 */
+	public void Gateway_invoice(Integer void){
+
+	}
+}//end Billing Service
---
+++ Archiect/Order Manager.java
@@ -0,0 +1,41 @@
+
+
+/**
+ * @author Kashmiri Queen
+ * @version 1.0
+ * @created 17-May-2025 01:39:23 Fatima
+ */
+public class Order Manager {
+
+	private <Billing_Service> Billing_Service;
+	private int Cust_ID;
+	private String Cust_Name;
+	private <Menu_Manager> Menu_manager;
+	private List MenuItem;
+	public Billing Service role b;
+
+	public Order Manager(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	/**
+	 * 
+	 * @param Parameter A
+	 * @param string
+	 */
+	public void Create_Order(int Parameter A, String string){
+
+	}
+
+	/**
+	 * 
+	 * @param Parameter
+	 * @param Para
+	 */
+	public void Gerneate_bill(int[] Parameter, Para){
+
+	}
+}//end Order Manager
