95baf70026814cd6d151ac07e0ce1243971f8e27
Tanzeela Fatima <149133736+Fatima-progmmer@users.noreply.github.com>
2025-06-14 08:34:28 +0500

    Add files via upload

---
+++ Archiect/All modules & operations/Billing Gateway.java
@@ -0,0 +1,35 @@
+
+
+/**
+ * @author Kashmiri Queen
+ * @version 1.0
+ * @created 17-May-2025 01:39:45 Fatima
+ */
+public class Billing Gateway {
+
+	private int Gateway;
+
+	public Billing Gateway(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	/**
+	 * 
+	 * @param A
+	 * @param B
+	 */
+	public void process_payment(int A, String B){
+
+	}
+
+	/**
+	 * 
+	 * @param A
+	 */
+	public void Transcation_validate(long A){
+
+	}
+}//end Billing Gateway
---
+++ Archiect/All modules & operations/Menu Manager.java
@@ -0,0 +1,37 @@
+
+
+/**
+ * @author Kashmiri Queen
+ * @version 1.0
+ * @created 17-May-2025 01:39:51 Fatima
+ */
+public class Menu Manager {
+
+	private List Menu_item;
+	public Menu Manager m_Menu Manager;
+
+	public Menu Manager(){
+
+	}
+
+	public void finalize() throws Throwable {
+
+	}
+	/**
+	 * 
+	 * @param A
+	 */
+	public void Get_available_item(array A){
+
+	}
+
+	/**
+	 * 
+	 * @param Name
+	 * @param Detail
+	 * @param Price
+	 */
+	public void Get_product_details(string Name, String Detail, int Price){
+
+	}
+}//end Menu Manager
